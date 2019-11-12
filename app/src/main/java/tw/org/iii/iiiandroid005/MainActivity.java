f99c85b3481a4bdb20b81b745951c30c501e8dbd
Seraphim <freedom1040425@gmail.com>
2019-11-12 11:09:13 +0800

    iiiAndroid005

--- app/src/main/java/tw/org/iii/iiiandroid005/MainActivity.java
+++ app/src/main/java/tw/org/iii/iiiandroid005/MainActivity.java
@@ -1,5 +1,6 @@
 package tw.org.iii.iiiandroid005;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -35,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         //測試
         for(int i=0;i<20;i++){
             HashMap<String,String> dd = new HashMap<>();
-            int rand =(int)(Math.random()*49+1);//列表亂數
+            int rand =(int)(Math.random()*49+100);//列表亂數
             dd.put(from[0],"Title"+rand);
             dd.put(from[1],"content"+rand);
             dd.put("detail","Detail"+rand);//列表內資料
@@ -49,14 +50,27 @@ public class MainActivity extends AppCompatActivity {
         listView.setAdapter(adapter);
         //哪個選項被點選
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
+            @Override //方法拉出
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Log.v("brad","pos"+position);
+               StringBuffer sb = new StringBuffer();
+               sb.append(data.get(position).get("title")+"\n");
+               sb.append(data.get(position).get("msg")+"\n");
+               sb.append(data.get(position).get("detail")+"\n");
+               dispalydetail(sb.toString());
+                // Log.v("brad","pos"+position);
             }
         });
+        //SimpleAdapter(this,null,
+        //null,R.layout.item_test1,null);
 
-                   //SimpleAdapter(this,null,
-                   //null,R.layout.item_test1,null);
+    }
+
+
+    private  void  dispalydetail(String msg){
+        new AlertDialog.Builder(this)
+                .setMessage(msg)
+                .create()
+                .show();
 
     }
 }
